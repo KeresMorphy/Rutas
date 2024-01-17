99a2c9861ff0b3c49047b68331ef5b720f050ef6
eddy <keresmorphy@gmail.com>
2024-01-17 17:48:00 -0600

    Hacer POST alta-clientes

--- src/app/alta-cliente/alta-cliente.page.ts
+++ src/app/alta-cliente/alta-cliente.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-alta-cliente',
   templateUrl: './alta-cliente.page.html',
   styleUrls: ['./alta-cliente.page.scss'],
 })
-export class AltaClientePage implements OnInit {
+export class AltaClientePage {
+  formData = {
+    CodCliente: '',
+    RazonSocial: '',
+    Domicilio: '',
+    NoExterior: '',
+    Ruta: '',
+    colonia: '',
+    Poblacion: '',
+    Estado: '',
+    Telefono: '',
+    CodPostal: '',
+    RFC: '',
+    Email: '',
+    FormaPago: '',
+    UsoCFDI: '',
+    EnviadoTaz: '',
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  submitForm() {
+    // Realiza la solicitud HTTP aquí
+    this.http.post('http://201.159.34.30:9295/bonnacarne-api/public/api/alta-clientes', this.formData)
+      .subscribe(response => {
+        console.log('Respuesta del servidor:', response);
+        // Puedes manejar la respuesta del servidor aquí
+      }, error => {
+        console.error('Error al enviar la solicitud:', error);
+        // Puedes manejar errores aquí
+      });
   }
-
 }
